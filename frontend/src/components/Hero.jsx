b30476aed134b6157dcc5810f0de4d90cf519603
dipesh <dipeshmahatto@gmail.com>
2024-10-07 23:11:42 +0545

    prata font added

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import {assets} from '../assets/assets'
+import { assets } from "../assets/assets";
 
 const Hero = () => {
   return (
     <div className="flex flex-col sm:flex-row border border-gray-400">
-        {/* Hero left side */}
+      {/* Hero left side */}
       <div className="w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0">
         <div className="text-[#414141]">
           <div className="flex items-center gap-2">
@@ -23,7 +23,6 @@ const Hero = () => {
       </div>
       {/* Hero Right side */}
       <img className="w-full sm:w-1/2" src={assets.hero_img} alt="" />
- 
     </div>
   );
 };
