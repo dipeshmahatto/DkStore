db30fdb3f9099e05d055aa932500bd52e1b43a81
dipesh <dipeshmahatto@gmail.com>
2024-10-13 20:28:50 +0545

    home and shop context is added

---
+++ frontend/src/components/LatestCollection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LatestCollection = () => {
+    const { products} = useContext(shopContext);
+    console.log(products)
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default LatestCollection
