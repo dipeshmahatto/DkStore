6d90eed368ee4a6e2e26fd66ea124d66fac2ace8
dipesh <dipeshmahatto@gmail.com>
2024-11-14 10:03:16 +0545

    about and contact page is added

--- frontend/src/components/NewsLetterBox.jsx
+++ frontend/src/components/NewsLetterBox.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const NewsLetterBox = () => {
   return (
-    <div className='text-center'>
-<p className='text-2xl font-medium text-gray-800'>Subscribe now & get 10% off</p>
+    <div className="text-center">
+      <p className="text-2xl font-medium text-gray-800">
+        Subscribe now & get 10% off
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default NewsLetterBox
+export default NewsLetterBox;
