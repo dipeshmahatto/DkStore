811f07268fdc64c24ccb04e3fc895982cbceb856
dipesh <dipeshmahatto@gmail.com>
2024-11-27 22:19:23 +0545

    list page is added

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -7,9 +7,10 @@ import List from "./pages/List";
 import Orders from "./pages/Orders";
 import Login from "./components/Login";
 import { ToastContainer } from 'react-toastify';
-  import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const backendUrl = import.meta.env.VITE_BACKEND_URL
+export const curreny = '$'
 
 const App = () => {
   const [token, setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):'');
