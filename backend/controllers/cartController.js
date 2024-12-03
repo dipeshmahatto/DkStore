c7e4bc52da912ea0ae70098f8449fdb2aff8e238
dipesh <dipeshmahatto@gmail.com>
2024-12-03 15:13:15 +0545

    cart auth is beging to add

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,16 @@
+
+//add products to user cart
+const addToCart = async  (req,res)=>{
+    
+
+}
+const updateCart = async  (req,res)=>{
+    
+
+}
+const getUserCart = async  (req,res)=>{
+    
+
+}
+
+export {addToCart, updateCart, getUserCart}
